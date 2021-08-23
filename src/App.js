c12677e4f468aef8455a738285c588520dc66dd2
bfsantos <brunafgdossantos@gmail.com>
2021-08-23 20:27:50 +0100

    change localhost

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://sheltered-wave-36136.herokuapp.com/imageurl', {
               method: 'post',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://sheltered-wave-36136.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
