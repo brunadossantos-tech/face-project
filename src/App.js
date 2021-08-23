358a3fa1d9134eca36e60d14542a539a93215893
bfsantos <brunafgdossantos@gmail.com>
2021-08-23 21:14:16 +0100

    correct small error

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('https://sheltered-wave-36136.herokuapp.com:3000/image', {
+          fetch('https://sheltered-wave-36136.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
